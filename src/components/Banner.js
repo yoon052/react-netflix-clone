6e7f9001bb8734ceb4b28584b0dcf1cf685db4c2
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-20 22:38:48 +0900

    feat: video banner
    if you click the play button, then the screen switches to video.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import requests from "../api/requests";
 import "./Banner.css";
+import styled from "styled-components";
 export default function Banner() {
   const [movie, setMovie] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
@@ -37,7 +38,7 @@ export default function Banner() {
       <header
         className="banner"
         style={{
-          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
           backgroundPosition: "top center",
           backgroundSize: "cover",
         }}
@@ -64,5 +65,51 @@ export default function Banner() {
         <div className="banner--fadeBottom" />
       </header>
     );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            title="YouTube video player"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
   }
 }
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
