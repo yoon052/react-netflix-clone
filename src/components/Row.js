4dc5440c0776180db157e353e19a97a5d8a255ca
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-20 23:51:09 +0900

    design: arrow

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,14 +4,15 @@ import "./Row.css";
 
 export default function Row({ isLargeRow, title, id, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     fetchMovieData();
   }, []);
 
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
+    console.log("request", request);
     setMovies(request.data.results);
-    return request;
   };
 
   return (
@@ -19,7 +20,14 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movie) => (
@@ -28,13 +36,20 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               src={`https://image.tmdb.org/t/p/original/${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
+              } `}
               alt={movie.name}
             />
           ))}
         </div>
-        <div className="" slider__arrow-right>
-          <span className="arrow">{">"}</span>
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
     </section>
