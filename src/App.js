32a6045ccaa09400d259a5aeb9da8f6aab9e33f6
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-20 23:39:30 +0900

    feat: row component
    arrange movie lists with poster

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import requests from "./api/requests";
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
