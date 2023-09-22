a8f4b4f2cbcce8f9cb0e522fa27cfac80dc99dc0
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-23 00:51:27 +0900

    feat: useDebounce
    make Custom Hooks input delay

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router";
 import axios from "../../api/axios";
 import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
 
 export default function SearchPage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -10,11 +11,12 @@ export default function SearchPage() {
   };
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async (searchTerm) => {
     try {
@@ -36,7 +38,7 @@ export default function SearchPage() {
             const movieImageUrl =
               "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
             return (
-              <div className="movie">
+              <div className="movie" key={movie.id}>
                 <div className="movie__column-poster">
                   <img
                     src={movieImageUrl}
@@ -52,7 +54,9 @@ export default function SearchPage() {
     ) : (
       <section className="no-results">
         <div className="no-results__text">
-          <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+          <p>
+            찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
+          </p>
         </div>
       </section>
     );
