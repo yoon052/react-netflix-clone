1785d9e1451855662c422ecad77f03ea0e831c05
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-23 20:31:22 +0900

    feat: modal window
    create custom hooks that cause modal windows to close when you click outside the modal window

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./MovieModal.css";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 function MovieModal({
   backdrop_path,
@@ -11,10 +12,15 @@ function MovieModal({
   vote_average,
   setModalOpen,
 }) {
+  const ref = useRef();
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
